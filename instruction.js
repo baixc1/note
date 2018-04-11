1e76d78b8ed7a80397d8659951bd747b1684a438
baixiaocheng <863562441@qq.com>
2018-04-11 23:37:02 +0800

    add instruction

---
+++ instruction.js
@@ -0,0 +1,11 @@
+windows
+打开当前目录文件夹  start .
+新建目录 mkdir(md) file 
+
+
+
+git 
+克隆代码
+a、git init
+b、git remote add origin git@github:server-name/repository.git
+c、git clone git@github:server-name/repository.git（或复制）
