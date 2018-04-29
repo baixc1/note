0a84e9b0d65a60d4131f5e8004857878fcaaf213
baixiaocheng <863562441@qq.com>
2018-04-29 22:38:29 +0800

    the note in April twenty-nine

--- books.js
+++ books.js
@@ -1,1 +1,11 @@
 《how browsers work》
+*Components: 
+*User Interface
+*Browser engine
+*Rendering engine
+	DOM tree -> Render tree -> Layout -> paint
+	gradual process
+	
+*Networking、
+*Javascript Interpreter
+*UI Backend、Data Storage
