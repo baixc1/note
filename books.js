21f796a33a7fce8f15398d5ca431b240588c30c5
baixiaocheng <863562441@qq.com>
2018-05-01 22:39:33 +0800

    the note of books in May-one

--- books.js
+++ books.js
@@ -3,8 +3,20 @@
 *User Interface
 *Browser engine
 *Rendering engine
-	DOM tree -> Render tree -> Layout -> paint
+	1、Parser 
+		Parsing - general
+		Parser:				Document -> Lexical Analysis -> Syntax Analysis -> Parse Tree
+		Translation:		Source Code -> Parsing -> Parse Tree ->Translation -> Machine Code
+		
+		HTML Parser
+		parsing:			Network -> Tokenizer -> Tree Construction -> DOM
+		tokenization:		Data State -> Tag open state -> Tag name state... -> End tag token -> 
+							Tag name state ... -> Data State
+		tree construction: 	initial mode -> before HTML -> before head -> in head -> after head ->
+							in body -> after body -> after after body
+	2、DOM tree -> Render tree -> Layout -> paint
 	gradual process
+	
 	
 *Networking、
 *Javascript Interpreter
