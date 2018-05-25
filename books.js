39cc38ad98003283867b01c381671cb79f9ae1f4
DESKTOP-4GL8UF8\Siyanhui <863562441@qq.com>
2018-05-25 18:36:12 +0800

    the note in May-twenty-five

--- books.js
+++ books.js
@@ -25,14 +25,41 @@
 				Scripts		synchronous		//no defer and no mark thread
 				Speculative parsing			main/thread parses		//scripts, style sheets and images
 				Style sheets				//The script does not affect the style
-				
-				
+		
 				
 	2、 -> Render tree 
 		RenderObject				//create a instance with attributes
 		render tree relation to the DOM tree
 		attachment				//"attach" method
-		Style Computation
+		Render tree construction
+			-Style Computation
+			-Sharing style data
+			-Division into structs
+			-Computing the style contexts using the rule tree
+			-Manipulating the rules for an easy match
+			-Style sheet cascade order
+			-Specifity
+		Layout
+			-Dirty bit system
+			-Global and incremental layout
+			-Asynchronous and Synchronous layout
+			-Optimizations
+			-The layout process
+			-Width calculation
+			-Line Breaking
+		Painting
+			-Global and Incremental
+			-The painting order
+			-Firefox display list
+			-Webkit rectangle storage
+			-Dynamic changes
+			-The rendering engine's threads
+		CSS2 visual model
+			-The canvas
+			-CSS Box model
+			-Positioning scheme
+			-Positioning
+			-Layered representation
 	
 	3、 -> Layout -> paint
 	gradual process
