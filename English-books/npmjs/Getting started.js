b539867dca37fefc5746abc41f08656ffacef869
wsadc <863562441@qq.com>
2018-09-03 18:36:49 +0800

    npm - getting started - September-three

---
+++ English-books/npmjs/Getting started.js
@@ -0,0 +1,6 @@
+1. What is npm?		
+	-packages
+	-the website     CLI(the Command Line Interface)		the registry(js software database)
+	-Use npm to . . .
+	-Where do I Start?
+	-Your First Package Page
