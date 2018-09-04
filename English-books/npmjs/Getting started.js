401aa6a8eae643e65234492073546e0e1776a4e2
wsadc <863562441@qq.com>
2018-09-04 17:52:35 +0800

    the npmjs in September-four

--- English-books/npmjs/Getting started.js
+++ English-books/npmjs/Getting started.js
@@ -4,3 +4,11 @@
 	-Use npm to . . .
 	-Where do I Start?
 	-Your First Package Page
+2. Finding and Selecting Packages
+	-criteria
+	-package page
+	-Parts   //Readme  Dependencies Dependents Versions
+3. new Account & install
+	-Create an account
+	-Verify email address
+	
