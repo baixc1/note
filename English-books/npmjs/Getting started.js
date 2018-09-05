c4d91f7c4048c8234ed40dec76c82c7dab24a499
wsadc <863562441@qq.com>
2018-09-05 14:50:18 +0800

    npmjs about account and install on September 5th

--- English-books/npmjs/Getting started.js
+++ English-books/npmjs/Getting started.js
@@ -10,5 +10,12 @@
 	-Parts   //Readme  Dependencies Dependents Versions
 3. new Account & install
 	-Create an account
-	-Verify email address
+	-Verify email address  //www.npmjs.com/~name
+	-After you set up your Login Account		//memu
+	-Terminals, Editors, and Git
+	-Install npm & manage npm versions
+	-Using a version manager to install Node.js and npm			//nvm - node version manager
+	-Login to npm from a terminal  	//npm login, npm whoami
+	-Learn more
+4. How to Install Local Packages
 	
