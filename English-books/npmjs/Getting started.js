91108dcdc9078a51fe1840b59199f5d7ec736421
wsadc <863562441@qq.com>
2018-09-06 14:01:16 +0800

    npmjs in September 6th

--- English-books/npmjs/Getting started.js
+++ English-books/npmjs/Getting started.js
@@ -18,4 +18,41 @@
 	-Login to npm from a terminal  	//npm login, npm whoami
 	-Learn more
 4. How to Install Local Packages
-	
+	-Installing a Package		//npm install <package-name>
+	-Using the Installed Package in Your Code	//1.require  2.node xx.js
+5. Working with package.json
+	-Requirements		//cli
+		-npm init
+		-npm init [--yes]|[-y]
+		-npm set init.[author/license...]
+		-.npm-init.js
+	-Specifying Dependencies	//dependencies, devDependencies
+		-npm install <package> [--save-prod]	//produce
+		-npm install <package> --save-dev		//develop
+		-Managing dependency versions
+		
+6. How to Update Local Packages		//npm update, npm outdated
+7. How to Uninstall Local Packages	//npm uninstall [--save] <package_name>
+8. How to Install Global Packages	//npm install -g <package_name>
+9. How to Update Global Packages
+		-npm update -g [<package_name>]
+		-npm outdated -g --depth=0
+10. How to uninstall global packages		//npm uninstall -g <package>
+11. How to Create Node.js Modules			//exports
+12. How to Publish & Update a Package  ?
+13. How to use Semantic Versioning
+	-Semver for Publishers
+14. How to Work with Scoped Packages
+
+
+
+
+
+
+
+
+
+
+
+
+
